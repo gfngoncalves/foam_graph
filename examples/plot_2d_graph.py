2fa00af39d5b86660b3bc5488ceba7453fdde8fe
Gabriel Goncalves <gabrielfng@hotmail.com>
2022-03-07 17:17:26 +0000

    fix: fixed name of plotting example scripts

--- examples/plot_2d_graph.py
+++ examples/plot_2d_graph.py
@@ -1,26 +1,28 @@
 # %%
 import numpy as np
+import matplotlib.pyplot as plt
 from torch_geometric.data import extract_tar
 from foam_graph.utils.graph_from_foam import read_case
-from foam_graph.visualization.graph_3d_plotting import plot_3d_graph
+from foam_graph.visualization.graph_plotting import plot_graph
 
 # %% Extract tar and read case as a graph
-extract_tar("angledDuct.tar.xz", ".", mode="r:xz")
+extract_tar("damBreak.tar.xz", ".", mode="r:xz")
 
 graph = read_case(
-    "angledDuct",
+    "damBreak",
     ("alpha.water",),
-    read_boundaries=False,
+    read_boundaries=True,
     times="first_and_last",
 )
 
-# %% Plot alpha field for 2D case
+#%% Plot alpha field for 2D case
 
-field_name = "alpha.water"
-field_component = 0
-time = 10
+field_name_plot = "alpha.water"
+field_component_plot = 0
+time = 1
 
-fig = plot_3d_graph(
-    graph, field_name=field_name, field_component=field_component, time=time
-)
-fig.show()
+fig, ax = plt.subplots(figsize=(10, 10))
+plot_graph(graph, field_name_plot, field_component_plot, time=time, ax=ax)
+
+plt.tight_layout()
+plt.show()
