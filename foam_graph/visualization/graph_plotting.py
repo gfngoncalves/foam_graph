d19daf975ae6f4ac2f5caa21ff28c76dbba66f94
Gabriel Goncalves <gabrielfng@hotmail.com>
2022-03-07 17:18:28 +0000

    fix: fixed bug on networkx conversion

--- foam_graph/visualization/graph_plotting.py
+++ foam_graph/visualization/graph_plotting.py
@@ -9,9 +9,7 @@ def plot_graph(graph, field_name=None, field_component=0, time=None, ax=None):
     field_name_with_time = field_name
     if time is not None and field_name is not None:
         field_name_with_time = f"{field_name_with_time}_time{time}"
-    node_attrs = (
-        ("pos", field_name_with_time) if field_name_with_time is not None else ("pos",)
-    )
+    node_attrs = ["pos", field_name_with_time] if field_name_with_time is not None else ["pos",]
     graphnx = to_networkx(graph, node_attrs=node_attrs, to_undirected=True)
     selected_nodes = [n for n, v in graphnx.nodes(data=True)]
     subgraphnx = graphnx.subgraph(selected_nodes)
