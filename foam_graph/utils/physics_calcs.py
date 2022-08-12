fix: fixed div calculation

foam_graph/utils/physics_calcs.py
@@ -9,19 +9,29 @@ def _mul_sparse_dense(m_sparse, m_dense):
 
 
 def _d_edge_attr_d_pos_node(graph: Data, j: int, indices: Tensor, i_mag_dx: int):
-    vals = graph.edge_attr[indices[0]]
-    v = torch.zeros_like(vals)
-    v[:, j] = 1
-    v[:, i_mag_dx] = vals[:, j] / vals[:, i_mag_dx]
-    return torch.sparse_coo_tensor(indices, v, graph.edge_attr.size())
+    vals = graph.edge_attr[indices]
+
+    idxs_dx = torch.full_like(vals[:, 0], j)
+    idxs_mag_dx = torch.full_like(vals[:, 0], i_mag_dx % vals.size(1))
+
+    idxs_dx_edges = torch.stack((indices, idxs_dx), 0)
+    idxs_mag_dx_edges = torch.stack((indices, idxs_mag_dx), 0)
+
+    vals_dx = -torch.ones_like(vals[:, 0])
+    vals_mag_dx = -vals[:, j] / vals[:, i_mag_dx]
+
+    v1 = torch.sparse_coo_tensor(idxs_dx_edges, vals_dx, graph.edge_attr.size())
+    v2 = torch.sparse_coo_tensor(idxs_mag_dx_edges, vals_mag_dx, graph.edge_attr.size())
+
+    return v1 + v2
 
 
 def _d_edge_attr_d_pos(graph: Data, i: int, j: int, i_mag_dx: int):
     indices_orig = torch.transpose((graph.edge_index[0, :] == i).nonzero(), 0, 1)
     indices_dest = torch.transpose((graph.edge_index[1, :] == i).nonzero(), 0, 1)
 
-    s_orig = _d_edge_attr_d_pos_node(graph, j, indices_orig, i_mag_dx)
-    s_dest = _d_edge_attr_d_pos_node(graph, j, indices_dest, i_mag_dx)
+    s_orig = _d_edge_attr_d_pos_node(graph, j, indices_orig[0], i_mag_dx)
+    s_dest = _d_edge_attr_d_pos_node(graph, j, indices_dest[0], i_mag_dx)
 
     s = s_dest - s_orig
     return s.coalesce().detach()
@@ -108,6 +118,6 @@ def div(
         s = _mul_sparse_dense(d_edge_attr_d_pos, d_edge_attr_norm_d_edge_attr)
         s = _mul_sparse_dense(s, d_y_norm_d_edge_attr_norm)
         s *= d_y_d_y_norm[i_y]
-        div_value += s.values().sum()
+        div_value += s.coalesce().values().sum()
 
     return div_value

test/test_div.py
@@ -19,16 +19,21 @@ def test_div():
     graph = transforms(g)
 
     wx = torch.tensor(3.0)
+    wy = torch.tensor(6.0)
     y1 = (
         0.5
         * wx
         * scatter_add(g.edge_attr[:, 0], g.edge_index[1], dim=0, dim_size=g.num_nodes)
     )
-    y2 = y1
+    y2 = (
+        0.5
+        * wy
+        * scatter_add(g.edge_attr[:, 1], g.edge_index[1], dim=0, dim_size=g.num_nodes)
+    )
     y = torch.column_stack((y1, y2))
 
     edge_normalize = NormalizeZScore("edge_attr", attr_mean=0)
     target_normalize = NormalizeZScore("y", attr_mean=0)
     div_loss = div(y, graph, 0, edge_normalize, target_normalize)
 
-    torch.testing.assert_close(div_loss.float(), wx.float())
+    torch.testing.assert_close(div_loss.float(), -wx.float()-wy.float())