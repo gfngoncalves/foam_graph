fix: fixed div calculation and added analytical result

foam_graph/utils/physics_calcs.py
@@ -17,7 +17,7 @@ def _d_edge_attr_d_pos_node(graph: Data, j: int, indices: Tensor, i_mag_dx: int)
     idxs_dx_edges = torch.stack((indices, idxs_dx), 0)
     idxs_mag_dx_edges = torch.stack((indices, idxs_mag_dx), 0)
 
-    vals_dx = -torch.ones_like(vals[:, 0])
+    vals_dx = torch.ones_like(vals[:, 0])
     vals_mag_dx = -vals[:, j] / vals[:, i_mag_dx]
 
     v1 = torch.sparse_coo_tensor(idxs_dx_edges, vals_dx, graph.edge_attr.size())

test/test_div.py
@@ -5,35 +5,32 @@ from torch_scatter import scatter_add
 from foam_graph.transforms import NormalizeZScore
 from foam_graph.utils.physics_calcs import div
 
-
 def test_div():
     pos = torch.tensor(
-        [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0],],
+        [[0.0, 0.0], [0.0, -1.0], [-1.0, 1.0], [1.0, 2.0],],
         dtype=torch.double,
         requires_grad=True,
     )
-    edges = torch.tensor([[0, 0, 1, 1, 2, 2, 3, 3], [1, 2, 0, 3, 0, 3, 1, 2],])
+    edges = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0],])
     g = Data(pos=pos, edge_index=edges)
 
     transforms = T.Compose([T.Cartesian(norm=False), T.Distance(norm=False),])
-    graph = transforms(g)
+    g = transforms(g)
 
-    wx = torch.tensor(3.0)
-    wy = torch.tensor(6.0)
-    y1 = (
-        0.5
-        * wx
-        * scatter_add(g.edge_attr[:, 0], g.edge_index[1], dim=0, dim_size=g.num_nodes)
-    )
-    y2 = (
-        0.5
-        * wy
-        * scatter_add(g.edge_attr[:, 1], g.edge_index[1], dim=0, dim_size=g.num_nodes)
-    )
+    wx = torch.tensor([1.69654, -1.40198, 1.20593])
+    k = -2.80395
+
+    y_target = torch.tensor([0, -3.0, 2.0, 1.0])
+    div_target = 5.40302
+
+    y1 = wx * scatter_add(g.edge_attr, g.edge_index[1], dim=0, dim_size=g.num_nodes)
+    y1 = torch.sum(y1, 1) + k
+    y2 = torch.zeros_like(y1)
     y = torch.column_stack((y1, y2))
 
     edge_normalize = NormalizeZScore("edge_attr", attr_mean=0.0)
     target_normalize = NormalizeZScore("y", attr_mean=0.0)
-    div_loss = div(y, graph, 0, edge_normalize, target_normalize)
+    div_loss = div(y, g, 0, edge_normalize, target_normalize)
 
-    torch.testing.assert_close(div_loss.float(), -wx.float()-wy.float())
+    torch.testing.assert_close(y1.float(), y_target, atol=1e-4, rtol=1e-4)
+    torch.testing.assert_close(div_loss.float(), torch.tensor(div_target))